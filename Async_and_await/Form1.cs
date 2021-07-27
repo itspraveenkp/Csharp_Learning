93cf004b5bb218d1bc03a9a6f40f17db2a783609
itspraveenkp <42871516+itspraveenkp@users.noreply.github.com>
2021-07-27 15:38:50 +0530

    Add files via upload

---
+++ Async_and_await/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Async_and_await
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int CountCharacters()
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(@"C:\data\Data.txt"))
+            {
+                string content = reader.ReadToEnd();
+                count = content.Length;
+                Thread.Sleep(5000);
+            }
+            return count;
+        }
+
+        private async void btnProcessFile_Click(object sender, EventArgs e)
+        {
+            Task<int> task = new Task<int>(CountCharacters);
+            task.Start();
+            lblCount.Text = "Processing File please wait...";
+            int count = await task;
+            lblCount.Text = count.ToString() + " Character in file";
+
+        }
+    }
+}
